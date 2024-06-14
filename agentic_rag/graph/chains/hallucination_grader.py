60854b466b212f34c42ca68373945d54e4d3d21f
gotsulyakk <igorgotsu@gmail.com>
2024-06-14 22:49:39 +0300

    fix duplication

--- agentic_rag/graph/chains/hallucination_grader.py
+++ agentic_rag/graph/chains/hallucination_grader.py
@@ -16,8 +16,6 @@ llm = ChatOpenAI(temperature=0)
 
 structured_llm_grader = llm.with_structured_output(GradeHallucinations)
 
-structured_llm_grader = llm.with_structured_output(GradeHallucinations)
-
 message = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n 
      Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
 hallucination_prompt = ChatPromptTemplate.from_messages(
